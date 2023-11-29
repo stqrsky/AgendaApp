108bfc115f5e1b2a26bec7308cb155f378abc28b
STQR <49653500+stqrsky@users.noreply.github.com>
2023-11-29 13:41:43 +0100

    set up project: connect firebase backend, add packages

--- AgendaApp/Other/AgendaAppApp.swift
+++ AgendaApp/Other/AgendaAppApp.swift
@@ -5,10 +5,15 @@
 //  Created by Starsky Menchate on 29.11.23.
 //
 
+import FirebaseCore
 import SwiftUI
 
 @main
 struct AgendaAppApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
