130833497050ee7ca8bf73315d0870efdc9f17cb
STQR <49653500+stqrsky@users.noreply.github.com>
2023-11-29 11:54:13 +0100

    Initial Commit

---
+++ AgendaApp/AgendaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgendaAppApp.swift
+//  AgendaApp
+//
+//  Created by Starsky Menchate on 29.11.23.
+//
+
+import SwiftUI
+
+@main
+struct AgendaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
