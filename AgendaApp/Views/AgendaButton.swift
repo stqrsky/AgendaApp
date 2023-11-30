e43446022cdb2b6f8da732f83513a286d335519c
STQR <49653500+stqrsky@users.noreply.github.com>
2023-11-30 13:05:54 +0100

    registration form, add custom button

---
+++ AgendaApp/Views/AgendaButton.swift
@@ -0,0 +1,18 @@
+//
+//  AgendaButton.swift
+//  AgendaApp
+//
+//  Created by Starsky Menchate on 30.11.23.
+//
+
+import SwiftUI
+
+struct AgendaButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AgendaButton()
+}
