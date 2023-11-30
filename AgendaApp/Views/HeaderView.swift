1304f707d103733c1a627a19a9007c38c3e00b8d
STQR <49653500+stqrsky@users.noreply.github.com>
2023-11-30 11:55:53 +0100

    build LoginView

---
+++ AgendaApp/Views/HeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderView.swift
+//  AgendaApp
+//
+//  Created by Starsky Menchate on 30.11.23.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderView()
+}
