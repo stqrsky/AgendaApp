92a29c34670ee8e96f95ea375ebe68efdc0fdf3c
STQR <49653500+stqrsky@users.noreply.github.com>
2023-12-04 19:11:21 +0100

    save() function, Model AgendaItem

--- AgendaApp/ViewModels/NewItemViewViewModel.swift
+++ AgendaApp/ViewModels/NewItemViewViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by Starsky Menchate on 30.11.23.
 //
 
+import Firebase
+import FirebaseFirestore
 import Foundation
 
 class NewItemViewViewModel: ObservableObject {
@@ -15,7 +17,33 @@ class NewItemViewViewModel: ObservableObject {
     init() {}
     
     func save() {
+        guard canSave else {
+            return
+        }
         
+        // Get current user id.
+        guard let uId = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        // Create model
+        let newId = UUID().uuidString
+        let newItem = AgendaItem(
+            id: newId,
+            title: title,
+            dueDate: dueDate.timeIntervalSince1970,
+            createdDate: Date().timeIntervalSince1970,
+            isDone: false
+        )
+        
+        // Save model to the database
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(uId)
+            .collection("agendas")
+            .document("newId")
+            .setData(newItem.asDictionary())
     }
     
     var canSave: Bool {
